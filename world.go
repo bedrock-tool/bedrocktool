8d4a7265f081a3dfe7d73acd11e6c5e6bfa01a1e
olebeck <31539311+olebeck@users.noreply.github.com>
2022-08-23 20:47:49 +0200

    update for new protocol

--- world.go
+++ world.go
@@ -165,7 +165,7 @@ func (w *WorldState) ProcessLevelChunk(pk *packet.LevelChunk) {
 	} else {
 		// request all the subchunks
 
-		var Offset_table = [][3]int8{
+		var Offset_table = []protocol.SubChunkOffset{
 			{0, 0, 0}, {0, 1, 0}, {0, 2, 0}, {0, 3, 0},
 			{0, 4, 0}, {0, 5, 0}, {0, 6, 0}, {0, 7, 0},
 			{0, 8, 0}, {0, 9, 0}, {0, 10, 0}, {0, 11, 0},
