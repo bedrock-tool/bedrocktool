384728101cdd8ae12fef2b638d5ca3109e044373
olebeck <31539311+olebeck@users.noreply.github.com>
2024-07-03 03:11:48 +0200

    refactor build script

--- build.py
+++ build.py
@@ -1,195 +1,176 @@
-import subprocess, re, sys, os, shutil, json, binascii, hashlib, gzip
-
-VER_RE = re.compile(r"v(\d\.\d+\.\d+)(?:-(\d+)-(\w))?")
+import subprocess, re, os, shutil, json, binascii, hashlib, gzip
+from collections import namedtuple
+import git
+from datetime import datetime
 
 NAME = "bedrocktool"
 APP_ID = "yuv.pink.bedrocktool"
-GIT_TAG = subprocess.run(["git", "describe", "--exclude", "r*", "--tags", "--always"], stdout=subprocess.PIPE).stdout.decode("utf8").split("\n")[0]
-if GIT_TAG == "":
-    GIT_TAG = "v0.0.0"
-VER_MATCH = VER_RE.match(GIT_TAG)
-VER = VER_MATCH.group(1)
-PATCH = VER_MATCH.group(2) or "0"
-TAG = f"{VER}-{PATCH}"
 
-print(f"VER: {VER}")
-print(f"TAG: {TAG}")
+VER_RE = re.compile(r"v(\d\.\d+\.\d+)(?:-(\d+)-(\w))?")
+
+Build = namedtuple("Build", ["os", "arch", "gui"])
 
 GITHUB_OUTPUT = os.getenv("GITHUB_OUTPUT")
 if GITHUB_OUTPUT:
-    with open(GITHUB_OUTPUT, "a") as f:
-        f.write(f"release_tag=r{VER}\n")
+    GITHUB_OUTPUT = open(GITHUB_OUTPUT, "a")
 
-with open("./subcommands/resourcepack-d/resourcepack-d.go", "rb") as f:
-    PACK_SUPPORT = f.read(100).count(b"package ") > 0
-print(f"Pack Support: {PACK_SUPPORT}")
-print(flush=True)
+def generate_changelog(tag_name):
+    repo = git.Repo(".")
+    
+    try:
+        tag = repo.tags[tag_name]
+    except IndexError:
+        raise ValueError(f"Tag {tag_name} does not exist in the repository")
 
-LDFLAGS = f"-s -w -X github.com/bedrock-tool/bedrocktool/utils/updater.Version={TAG}"
+    commits_since_tag = list(repo.iter_commits(rev=f'{tag_name}..HEAD'))
 
-PLATFORMS = [
-    ("windows", ["amd64"], ".exe"),
-    ("linux", ["amd64"], ""),
-    #("darwin", ["amd64", "arm64"], ""),
-    #("android", ["arm64"], ".apk"),
-    #("js", ["wasip1"], "")
-]
+    if not commits_since_tag:
+        return f"No commits found since tag {tag_name}"
+
+    changelog = []
+    for commit in commits_since_tag:
+        changelog.append(f"- {commit.hexsha[:7]} {commit.message.strip()}")
+
+    return '\n'.join(changelog)
 
 
-def clean():
-    shutil.rmtree("./tmp", True)
-    shutil.rmtree("./builds", True)
-    shutil.rmtree("./updates", True)
+def get_version():
+    GIT_TAG = subprocess.run(["git", "describe", "--exclude", "r*", "--tags", "--always"], stdout=subprocess.PIPE).stdout.decode("utf8").split("\n")[0]
+    if GIT_TAG == "":
+        GIT_TAG = "v0.0.0"
+    VER_MATCH = VER_RE.match(GIT_TAG)
+    VER = VER_MATCH.group(1)
+    PATCH = VER_MATCH.group(2) or "0"
+    TAG = f"{VER}-{PATCH}"
+    if GITHUB_OUTPUT:
+        GITHUB_OUTPUT.write(f"release_tag=r{VER}\n")
+    return VER, TAG
+
+
+def check_pack_support():
+    with open("./subcommands/resourcepack-d/resourcepack-d.go", "rb") as f:
+        return f.read(100).count(b"package ") > 0
+
+
+def clean_syso():
     for file in os.listdir("./cmd/bedrocktool"):
         if file.endswith(".syso"):
             os.remove(f"./cmd/bedrocktool/{file}")
 
-def make_dirs():
-    os.makedirs("./tmp", exist_ok=True)
-    os.makedirs("./builds", exist_ok=True)
-    os.makedirs("./updates", exist_ok=True)
 
+def do_build(build: Build):
+    print("Building", build)
+    env = dict()
+    env.update(os.environ.copy())
+    env["GOVCS"] = "*:off"
 
-def build_cli(platform: str, arch: str, env_in: dict[str,str], tags: list[str], ldflags, compiled_path: str):
-    env = {}
-    env.update(env_in)
-    env.update({
-        "GOOS": platform,
-        "GOARCH": arch,
-    })
-    args = [
-        "go", "build",
-        "-ldflags", ldflags,
-        "-trimpath",
-        "-tags", ",".join(tags),
-        "-o", compiled_path,
-        "-v"
-    ]
+    tags = list()
+    if PACK_SUPPORT:
+        tags.append("packs")
+
+    ldflags = list()
+    ldflags.append(f"-s -w -X github.com/bedrock-tool/bedrocktool/utils/updater.Version={TAG}")
+
+    if build.gui:
+        CmdName = "bedrocktool-gui"
+        tags.append("gui")
+    else:
+        CmdName = "bedrocktool"
+    ldflags.append(f"-X github.com/bedrock-tool/bedrocktool/utils/updater.CmdName={CmdName}")
+
+    ext = {
+        "windows": ".exe",
+        "linux": ""
+    }[build.os]
+
+    compiled_path = f"./builds/{NAME}-{build.os}-{build.arch}-{TAG}{'-gui' if build.gui else ''}{ext}"
+
+    if build.gui and build.os != "linux":
+        clean_syso()
+        if build.os == "windows":
+            ldflags.append("-H=windows")
+
+        args = [
+            "gogio",
+            "-arch", build.arch,
+            "-target", build.os,
+            "-icon", "icon.png",
+            "-tags", ",".join(tags),
+            "-ldflags", " ".join(ldflags),
+            "-o", compiled_path,
+            "-x"
+        ]
+        if build.os in ["android", "ios"]:
+            args.extend(["-appid", APP_ID])
+    else:
+        env.update({
+            "GOOS": build.os,
+            "GOARCH": build.arch
+        })
+        args = [
+            "go", "build",
+            "-ldflags", " ".join(ldflags),
+            "-trimpath",
+            "-tags", ",".join(tags),
+            "-o", compiled_path,
+            "-v"
+        ]
+
     args.append("./cmd/bedrocktool")
+    subprocess.run(args, env=env).check_returncode()
+    if build.gui and build.os == "windows":
+        clean_syso()
 
-    env2 = os.environ.copy()
-    env2.update(env)
-    subprocess.run(args, env=env2).check_returncode()
-
-
-def build_gui(platform: str, arch: str, env, tags: list[str], ldflags, compiled_path: str):
-    if platform == "windows":
-        ldflags += " -H=windows"
-
-    args = [
-        "gogio",
-        "-arch", arch,
-        "-target", platform,
-        "-icon", "icon.png",
-        "-tags", ",".join(tags),
-        "-ldflags", ldflags,
-        "-o", compiled_path,
-        "-x"
-    ]
-    if platform in ["android", "ios"]:
-        args.extend(["-appid", APP_ID])
-    args.append("./cmd/bedrocktool")
-
-    env2 = os.environ.copy()
-    env2.update(env)
-    subprocess.run(args, env=env2).check_returncode()
-
-
-def package(platform: str, arch: str, compiled_path: str, GUI: bool, ext: str):
-    SUB1 = '-gui' if GUI else ''
-    exe_out_path = f"./builds/{NAME}-{platform}-{arch}-{TAG}{SUB1}{ext}"
-
-    if platform == "js":
-        if GUI:
-            shutil.copytree(compiled_path, exe_out_path)
-        else:
-            shutil.copy(compiled_path, exe_out_path)
-        return
-
-    # create hash and copy
-    with open(compiled_path, "rb") as f:
-        exe_data = f.read()
-        sha = binascii.b2a_base64(hashlib.sha256(exe_data).digest()).decode("utf8").split("\n")[0]
-    shutil.copy(compiled_path, exe_out_path)
+    exe_hash = sha256_file(compiled_path)
 
     # create update
-    updates_dir = f"./updates/{NAME}{SUB1}"
+    updates_dir = f"./updates/{NAME}{'-gui' if build.gui else ''}"
     os.makedirs(updates_dir, exist_ok=True)
-    with open(f"{updates_dir}/{platform}-{arch}.json", "w") as f:
+    with open(f"{updates_dir}/{build.os}-{build.arch}.json", "w") as f:
         f.write(json.dumps({
             "Version": TAG,
-            "Sha256": sha,
+            "Sha256": exe_hash,
         }, indent=2))
     
     # write update data
     os.makedirs(f"{updates_dir}/{TAG}", exist_ok=True)
-    with gzip.open(f"{updates_dir}/{TAG}/{platform}-{arch}.gz", "wb") as f:
-        f.write(exe_data)
-
-    os.remove(compiled_path)
-
-
-def build_all(platform_filter: str, arch_filter: str):
-    for (platform, archs, ext) in PLATFORMS:
-        if platform_filter and platform_filter != platform:
-            continue
-        archs = [a for a in archs if arch_filter == "" or a == arch_filter]
-        if len(archs) == 0:
-            continue
-        for GUI in [False, True]:
-            if platform in ["android", "js"] and not GUI:
-                continue
-
-            print(f"Building {platform} gui: {GUI}")
-            SUB1 = '-gui' if GUI else ''
-            name = f"{NAME}{SUB1}"
-
-            tags = []
-            if PACK_SUPPORT:
-                tags.append("packs")
-            if GUI:
-                tags.append("gui")
-
-            env = {
-                "GOVCS": "*:off"
-            }
-
-            ldflags = LDFLAGS
-            if tags.count("gui"):
-                CmdName = "bedrocktool-gui"
-            else:
-                CmdName = "bedrocktool"
-            ldflags += f" -X github.com/bedrock-tool/bedrocktool/utils/updater.CmdName={CmdName}"
-
-
-            for arch in archs:
-                compiled_path = f"./tmp/{platform}-{arch}{SUB1}/{name}{ext}"
-                os.makedirs(os.path.dirname(compiled_path), exist_ok=True)
-
-                if GUI and platform != "linux":
-                    build_gui(platform, arch, env, tags, ldflags, compiled_path)
-                else:
-                    build_cli(platform, arch, env, tags, ldflags, compiled_path)
-                
-                package(platform, arch, compiled_path, GUI, ext)
+    with gzip.open(f"{updates_dir}/{TAG}/{build.os}-{build.arch}.gz", "wb") as f:
+        with open(compiled_path, "rb") as fr:
+            f.write(fr.read())
 
 
 def main():
-    platform_filter = ""
-    arch_filter = ""
-    if len(sys.argv) > 1:
-        platform_filter = sys.argv[1]
+    os.makedirs("./builds", exist_ok=True)
+    os.makedirs("./updates", exist_ok=True)
 
-    if len(sys.argv) > 2:
-        arch_filter = sys.argv[2]
-    
-    if platform_filter == "clean":
-        clean()
-        return
+    builds = [
+        Build("windows", "amd64", True),
+        Build("linux", "amd64", True),
+        Build("windows", "amd64", False),
+        Build("linux", "amd64", False)
+    ]
 
-    clean()
-    make_dirs()
-    build_all(platform_filter, arch_filter)
+    shutil.rmtree("./updates", True)
+    for build in builds:
+        do_build(build)
+    changelog = generate_changelog("v"+VER)
+    print(changelog)
+    with open("./changelog.txt", "w") as f:
+        f.write("## Commits\n")
+        f.write(changelog)
 
+
+def sha256_file(path: str) -> str:
+    with open(path, "rb") as f:
+        exe_data = f.read()
+        return binascii.b2a_base64(hashlib.sha256(exe_data).digest()).decode("utf8").split("\n")[0]
+
+
+VER, TAG = get_version()
+print(f"VER: {VER}")
+print(f"TAG: {TAG}")
+
+PACK_SUPPORT = check_pack_support()
+print(f"Pack Support: {PACK_SUPPORT}")
 
 main()
