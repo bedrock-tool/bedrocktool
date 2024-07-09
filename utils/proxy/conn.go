b018dfe2f10632362703b822c2a7bbaa2c423e28
olebeck <31539311+olebeck@users.noreply.github.com>
2024-07-09 17:57:26 +0200

    fix url download, proper fix for mtu

--- utils/proxy/conn.go
+++ utils/proxy/conn.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net"
-	"strings"
 	"time"
 
 	"github.com/bedrock-tool/bedrocktool/locale"
@@ -80,13 +79,7 @@ func (p *Context) connectServer(ctx context.Context) (err error) {
 		return err
 	}
 
-	// server says it can handle 1400 mtu, it cant
-	var network = "raknet"
-	if strings.Contains(p.serverAddress, "enchanted.gg") {
-		network = "raknet-1200"
-	}
-
-	server, err := d.DialContext(ctx, network, p.serverAddress, 10*time.Second)
+	server, err := d.DialContext(ctx, "raknet", p.serverAddress, 10*time.Second)
 	if err != nil {
 		return err
 	}
