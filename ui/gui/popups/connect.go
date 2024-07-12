7e90c037d5c4ef51df967a0ce09406c2b5ff6742
olebeck <31539311+olebeck@users.noreply.github.com>
2024-07-12 19:57:53 +0200

    add a button to do netisolation fix

--- ui/gui/popups/connect.go
+++ ui/gui/popups/connect.go
@@ -2,6 +2,8 @@ package popups
 
 import (
 	"fmt"
+	"os/exec"
+	"runtime"
 
 	"gioui.org/layout"
 	"gioui.org/widget"
@@ -24,7 +26,8 @@ type ConnectPopup struct {
 	ListenIP   string
 	ListenPort int
 
-	connectButton widget.Clickable
+	connectButton      widget.Clickable
+	netIsolationButton widget.Clickable
 }
 
 func NewConnect(ui ui.UI, ListenIP string, ListenPort int) Popup {
@@ -38,9 +41,38 @@ func (p *ConnectPopup) ID() string {
 	return "connect"
 }
 
+func fixCheckNetIsolation() error {
+	command := `CheckNetIsolation LoopbackExempt -a -n=\"Microsoft.MinecraftUWP_8wekyb3d8bbwe\"; Write-Host -NoNewLine \"Done, Enter to Close\"; $null = $Host.UI.RawUI.ReadKey(\"NoEcho,IncludeKeyDown\");`
+	psCommand := fmt.Sprintf(`Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "%s"' -Verb RunAs`, command)
+	cmd := exec.Command("powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command", psCommand)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Error: %s\nOutput: %s\n", err, string(output))
+		return err
+	}
+
+	fmt.Println(string(output))
+	return nil
+}
+
 func (p *ConnectPopup) Layout(gtx C, th *material.Theme) D {
 	if p.connectButton.Clicked(gtx) {
-		utils.OpenUrl(fmt.Sprintf("minecraft://connect/?serverUrl=%s&serverPort=%d", p.ListenIP, p.ListenPort))
+		go utils.OpenUrl(fmt.Sprintf("minecraft://connect/?serverUrl=%s&serverPort=%d", p.ListenIP, p.ListenPort))
+	}
+
+	if p.netIsolationButton.Clicked(gtx) {
+		go func() {
+			err := fixCheckNetIsolation()
+			if err != nil {
+				messages.Router.Handle(&messages.Message{
+					Source: p.ID(),
+					Target: "ui",
+					Data:   messages.Error(err),
+				})
+			}
+		}()
+
 	}
 
 	if p.close.Clicked(gtx) {
@@ -86,7 +118,7 @@ func (p *ConnectPopup) Layout(gtx C, th *material.Theme) D {
 				return layout.Flex{
 					Axis:      layout.Horizontal,
 					Spacing:   layout.SpaceBetween,
-					Alignment: layout.Middle,
+					Alignment: layout.End,
 				}.Layout(gtx,
 					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
 						b := material.Button(th, &p.close, "Close")
@@ -95,9 +127,28 @@ func (p *ConnectPopup) Layout(gtx C, th *material.Theme) D {
 					}),
 					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
 						if p.state == "listening" {
-							b := material.Button(th, &p.connectButton, "Connect Minecraft")
-							b.CornerRadius = 8
-							return b.Layout(gtx)
+							var elems []layout.FlexChild = []layout.FlexChild{
+								layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+									b := material.Button(th, &p.connectButton, "Open Minecraft")
+									b.CornerRadius = 8
+									return b.Layout(gtx)
+								}),
+							}
+							if runtime.GOOS == "windows" {
+								elems = append(elems, []layout.FlexChild{
+									layout.Rigid(layout.Spacer{Width: 8}.Layout),
+									layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+										b := material.Button(th, &p.netIsolationButton, "LoopbackExempt")
+										b.CornerRadius = 8
+										return b.Layout(gtx)
+									}),
+								}...)
+							}
+
+							return layout.Flex{
+								Axis:      layout.Horizontal,
+								Alignment: layout.Middle,
+							}.Layout(gtx, elems...)
 						}
 						return layout.Dimensions{}
 					}),
