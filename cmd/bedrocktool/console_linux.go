865f6791bd58165d87eb6aacff39aaaf6c8522b7
olebeck <31539311+olebeck@users.noreply.github.com>
2025-10-08 13:23:48 +0200

    use dup3

--- cmd/bedrocktool/console_linux.go
+++ cmd/bedrocktool/console_linux.go
@@ -9,7 +9,7 @@ import (
 )
 
 func redirectStderr(f *os.File) {
-	err := syscall.Dup2(int(f.Fd()), int(os.Stderr.Fd()))
+	err := syscall.Dup3(int(f.Fd()), int(os.Stderr.Fd()), 0)
 	if err != nil {
 		log.Fatalf("Failed to redirect stderr to file: %v", err)
 	}
